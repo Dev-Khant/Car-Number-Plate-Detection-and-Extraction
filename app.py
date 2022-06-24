8dc173da14536286d42a7262cc0dc4d1e18a98f2
Dev Khant <devkhant24@gmail.com>
2022-06-24 23:22:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from PIL import Image
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 from flask import Flask, render_template, request, jsonify, session
-from functions import get_labels, get_prediction, load_model
-from extraction import crop_and_extract
+from utils.functions import get_labels, get_prediction, load_model
+from utils.extraction import crop_and_extract
 
 config_path = 'model\yolov4-custom.cfg'
 weights_path = 'model\custom.weights'
