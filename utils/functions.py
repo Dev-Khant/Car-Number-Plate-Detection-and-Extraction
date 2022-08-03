def34121fcee59bc672e5cc829042998fe955933
Dev Khant <devkhant24@gmail.com>
2022-08-03 17:46:20 +0530

    Update functions.py

--- utils/functions.py
+++ utils/functions.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+import boto3
 
 def get_labels(labels_path):
     # get the label class
@@ -11,10 +12,21 @@ def get_labels(labels_path):
 #     color = np.full((len(labels), 3),[0, 0, 255], dtype='uint8')
 #     return color
 
-def load_model(config_path, weights_path):
+def load_model(config_path):
+
+    print('========== Downloading Model ==========')
+    session = boto3.Session(
+            aws_access_key_id = 'AKIAQCDD7DGQCQ5KH5VP',
+            aws_secret_access_key = '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'
+        )
+
+    s3 = session.resource('s3')
+    s3.Bucket('car-plate-extractor').download_file('custom.weights', 'new.weights')
+    print('========== Model Downloaded ==========')
+
     # load yolov4 model
     print('========== Loading Model ==========')
-    net = cv2.dnn.readNet(weights_path, config_path)
+    net = cv2.dnn.readNet('new.weights', config_path)
     model = cv2.dnn_DetectionModel(net)
 
     model.setInputParams(size=(416, 416), scale = 1/ 255)
